cd9ac4f328e5aca81d90494069561f42d4884e3c
Sultan <skydyrbaev66@gmail.com>
2025-06-13 01:37:06 +0500

    new feature for dangerous places

--- users/DangerousPlace.go
+++ users/DangerousPlace.go
@@ -3,21 +3,22 @@ package users
 import "time"
 
 type CrimeReport struct {
-	ID          uint      `gorm:"primaryKey"`
-	Type        string
-	Article     string
-	Severity    string
-	Region      string
-	Street      string
-	House       string
-	PlaceType   string
-	Target      string
-	Department  string
-	CrimeDate   time.Time
-	KUSINumber  string
-	Latitude    float64
-	Longitude   float64
-	CreatedAt   time.Time
+	ID         uint      `gorm:"primaryKey" json:"id"`
+	Type       string    `json:"type"`
+	Article    string    `json:"article"`
+	Severity   string    `json:"severity"`
+	Region     string    `json:"region"`
+	Street     string    `json:"street"`
+	House      string    `json:"house"`
+	PlaceType  string    `json:"place_type"`
+	Target     string    `json:"target"`
+	Department string    `json:"department"`
+	CrimeDate  time.Time `json:"crime_date"`
+	KUSINumber string    `json:"kusi_number"`
+	Latitude   float64   `json:"latitude"`
+	Longitude  float64   `json:"longitude"`
+	CreatedAt  time.Time `json:"createdAt"`
 }
+
 
 
